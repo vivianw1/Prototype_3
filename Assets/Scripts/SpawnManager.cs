9efcf0fc2f4063cfa9a7b3393a4e1e576502d175
vivianw1 <viwu1@s.sfusd.edu>
2023-02-16 16:04:45 -0800

    3.1 finished

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
-    private Vector3 spawnPos = new Vector3(25,0,0);
+    private Vector3 spawnPos = new Vector3 (25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2; 
     // Start is called before the first frame update
     void Start()
-    {invokeRepeating
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
        
     }
 
