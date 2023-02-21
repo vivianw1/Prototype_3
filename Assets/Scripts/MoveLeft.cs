38b656bd064687a4c18f16ec8e4d8c7149cbc9c2
vivianw1 <viwu1@s.sfusd.edu>
2023-02-21 11:20:54 -0800

    3.2 finished

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 10;
+    private PlayerController1 playerController1Script;
+    private float leftBound = -15;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerController1Script = GameObject.Find("Player").GetComponent<PlayerController1>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       transform.Translate(Vector3.left * Time.deltaTime * speed); 
+        if(playerController1Script.gameOver==false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed); 
+        }   
+        if(transform.position.x< leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/PlayerController1.cs
+++ Assets/Scripts/PlayerController1.cs
@@ -5,30 +5,43 @@ using UnityEngine;
 public class PlayerController1 : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver;
 
     // Start is called befire the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     //Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)&& isOnGround)
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+            playerAnim.SetBool("Death_b",true);
+            playerAnim.SetInteger("DeathType_int",1);
+        }
     }
 }
 
