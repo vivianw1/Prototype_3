b45c880667ee924e29c8a02632f3dc832d76c568
vivianw1 <viwu1@s.sfusd.edu>
2023-02-22 11:21:38 -0800

    Play the particle on collision + others

--- Assets/Scripts/PlayerController1.cs
+++ Assets/Scripts/PlayerController1.cs
@@ -6,6 +6,7 @@ public class PlayerController1 : MonoBehaviour
 {
     private Rigidbody playerRb;
     private Animator playerAnim;
+    public ParticleSystem explosionParticle;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -41,6 +42,7 @@ public class PlayerController1 : MonoBehaviour
             gameOver = true;
             playerAnim.SetBool("Death_b",true);
             playerAnim.SetInteger("DeathType_int",1);
+            explosionParticle.Play();
         }
     }
 }
