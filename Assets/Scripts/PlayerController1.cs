c2b2dd565d069368fd7d7d974e19a2e2fd6db18f
vivianw1 <viwu1@s.sfusd.edu>
2023-02-27 11:25:15 -0800

    finished

--- Assets/Scripts/PlayerController1.cs
+++ Assets/Scripts/PlayerController1.cs
@@ -6,6 +6,7 @@ public class PlayerController1 : MonoBehaviour
 {
     private Rigidbody playerRb;
     private Animator playerAnim;
+    private AudioSource playerAudio;
     public ParticleSystem explosionParticle;
     public ParticleSystem dirtParticle;
     public AudioClip jumpSound;
@@ -20,6 +21,7 @@ public class PlayerController1 : MonoBehaviour
     {
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
         Physics.gravity *= gravityModifier;
     }
 
@@ -32,6 +34,7 @@ public class PlayerController1 : MonoBehaviour
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
             dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
 
@@ -49,6 +52,7 @@ public class PlayerController1 : MonoBehaviour
             playerAnim.SetInteger("DeathType_int",1);
             explosionParticle.Play();
             dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound,1.0f);
         }
     }
 }
